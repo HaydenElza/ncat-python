0b324e15296e36aaf8850700c5c9aae025d0a1e0
Hayden Elza <HaydenElza@users.noreply.github.com>
2021-12-09 16:18:29 -0600

    release 0.1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='ncat',
     packages=find_packages(include=['ncat']),
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper for NGS Coordinate Conversion and Transformation Tool (NCAT) API.',
     author='Hayden Elza',
     url='https://github.com/HaydenElza/ncat-python',
-    download_url='https://github.com/HaydenElza/ncat-python/archive/refs/tags/ncat-0.1.0.tar.gz',
+    download_url='https://github.com/HaydenElza/ncat-python/archive/refs/tags/ncat-0.1.1.tar.gz',
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner'],
